9d36f5affb2d7da74b977ab8381431b02a2a4d8e
Boris <borisalekseev95@gmail.com>
2025-06-03 21:51:51 +0200

    add bin/

---
+++ bin/gendiff.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1>', 'first configuration file')
+  .argument('<filepath2>', 'second configuration file')
+  .action((filepath1, filepath2, options) => {
+    const result = getDiff(filepath1, filepath2, options.format);
+    console.log(result);
+  });
+
+program.parse();
